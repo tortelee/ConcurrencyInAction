145b812662281f541e50376336a2127214187c99
tortelee <281617953@qq.com>
2024-12-27 18:39:13 +0800

    example in the chapter01

---
+++ Chapter01/helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void hello()
+{
+    std::cout << "Hello Concurrent World\n";
+}
+
+int main()
+{
+    thread t(hello);
+    t.join();
+    
+    return 0;
+}
