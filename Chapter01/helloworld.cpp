68f970d7835968fba85f4fdd14510dc3a0638648
tortelee <281617953@qq.com>
2024-12-27 18:43:36 +0800

    add paramters to the thread

--- Chapter01/helloworld.cpp
+++ Chapter01/helloworld.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <thread>
+#include <string>
 
 using namespace std;
 
-void hello()
+void hello(string words)
 {
     std::cout << "Hello Concurrent World\n";
+    std::cout << words << std::endl;
 }
 
 int main()
 {
-    thread t(hello);
+    string words = "I am the input the parameter";
+    thread t(hello, words);
     t.join();
-    
+
     return 0;
 }
