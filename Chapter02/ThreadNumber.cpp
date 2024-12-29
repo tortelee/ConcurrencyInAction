f0afa5a07f7d217ac4d9d86cac4ee816aca0461b
tortelee <281617953@qq.com>
2024-12-29 20:28:10 +0800

    threadNumber

---
+++ Chapter02/ThreadNumber.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <thread>
+
+// int main()
+// {
+//     std::cout<<"The number of threads that can be run concurrently is: "<<
+//     std::thread::hardware_concurrency()<<std::endl;
+//     return 0;
+// }
